c4921c7445ef8276ff82de66cca431c641ebd01b
dmitryom <148622553+dmitryom@users.noreply.github.com>
2023-12-28 21:40:22 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -62,6 +62,12 @@ col3.metric("Индекс доступности инфраструктуры", 
 col4.metric("Тренд", "4%", "100%")
 
 
+col5, col6, col7, col8 = st.columns(4)
+col5.metric("ROI", "20%", "4%", font_size=18)
+col6.metric("Индекс транспортной доступности", "5", "10", font_size=18) 
+col7.metric("Индекс доступности инфраструктуры", "6", "10", font_size=18)
+col8.metric("Тренд", "4%", "100%", font_size=18)
+
 # Отображение характеристик выбранной квартиры
 selected_flat = data[data['id'] == selected_flat_id].squeeze()
 st.subheader(f'Основные характеристики квартиры {selected_flat_id}')
