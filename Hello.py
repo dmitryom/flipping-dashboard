01294999c57bdaf8f3328dd4a06d692fca445aad
dmitryom <148622553+dmitryom@users.noreply.github.com>
2024-01-18 11:34:43 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,43 +28,6 @@ from streamlit_extras.metric_cards import style_metric_cards
 st.set_page_config(layout='wide')
 LOGGER = get_logger(__name__)
 
-
-def style_metric_cards(
-    color:str = "#232323",
-    background_color: str = "#FFF",
-    border_size_px: int = 1,
-    border_color: str = "#CCC",
-    border_radius_px: int = 5,
-    border_left_color: str = "#9AD8E1",
-    box_shadow: bool = True,
-):
-
-    box_shadow_str = (
-        "box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;"
-        if box_shadow
-        else "box-shadow: none !important;"
-    )
-    st.markdown(
-        f"""
-        <style>
-            div[data-testid="metric-container"] {{
-                background-color: {background_color};
-                border: {border_size_px}px solid {border_color};
-                padding: 5% 5% 5% 10%;
-                border-radius: {border_radius_px}px;
-                border-left: 0.5rem solid {border_left_color} !important;
-                color: {color};
-                {box_shadow_str}
-            }}
-             div[data-testid="metric-container"] p {{
-              color: {color};
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-
 # Загрузка данных из CSV___
 @st.cache
 def load_data():
