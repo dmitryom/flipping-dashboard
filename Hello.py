b18bd23db44cb27244e78ce731c2986295cc8531
dmitryom <148622553+dmitryom@users.noreply.github.com>
2024-01-18 16:59:33 +0300

    fix 18

--- Hello.py
+++ Hello.py
@@ -214,7 +214,8 @@ yandex_map_html = f"""
         var selectedMarker = new ymaps.Placemark([ {location_yandex_map[0]}, {location_yandex_map[1]} ], {{
             iconContent: 'Щелкни по мне правой кнопкой мыши!',
             preset: 'islands#redStretchyIcon',
-            preset: 'islands#darkGreenStretchyIcon',
+            iconContent: "Узнать адрес",
+            hintContent: "Перетащите метку и кликните, чтобы узнать адрес"
             balloonContent: '<strong>Selected Property</strong><br/>Cost per sq.m.: {selected_flat["price_sq"]} rub',
             balloonContentHeader: 'Selected Property',
             balloonContentBody: 'Cost per sq.m.: {selected_flat["price_sq"]} rub',
